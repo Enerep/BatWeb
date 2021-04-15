fb42a0b350086ce8c73e9765c962f36cd6ac8c4c
Bat-Ochir <47132106+Enerep@users.noreply.github.com>
2021-04-15 16:23:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 //All in html :/
-alert("Total count: (isset($_GET['token']) && preg_match('/^[0-9a-f]{8}$/', $_GET['token'])))")
+//alert("Total count: (isset($_GET['token']) && preg_match('/^[0-9a-f]{8}$/', $_GET['token'])))")
 
